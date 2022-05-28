8e06123f04e8a02ca065531b867c6a0e11d3f2c4
ali-aboubacar <ali.02.aboubacar@gmail.com>
2022-05-28 16:32:39 +0200

    affichage du cataloque et dans la page produit

---
+++ front/js/home.js
@@ -0,0 +1,15 @@
+fetch("http://localhost:3000/api/products")
+  .then((data) => data.json())
+  .then((jsonListProduct) => {
+    for (let jsonProduct of jsonListProduct) {
+      let product = new Product(jsonProduct);
+      document.getElementById("items").innerHTML += `
+      <a href="./product.html?id=${product._id}">
+            <article>
+              <img src=${product.imageUrl} alt=${product.altTxt}>
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>
+            </article>
+          </a>`;
+    }
+  });
---
+++ front/js/productManager.js
@@ -0,0 +1,29 @@
+// Recupere la chaine de caractere dan l'url
+const queryString_urlId = window.location.search;
+//console.log(queryString_urlId);
+
+//methode 1 pour extriare juste l'id
+//const id = queryString_urlId.slice(4);
+//console.log(id);
+const urlSearchParams = new URLSearchParams(queryString_urlId);
+//console.log(urlSearchParams);
+
+const id = urlSearchParams.get("id");
+
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then((data) => data.json())
+  .then((singleProduct) => {
+    if (id == singleProduct._id) {
+      document.querySelector("#title").innerText += `${singleProduct.name}`;
+      document.querySelector(
+        ".item__img"
+      ).innerHTML += `<img src="${singleProduct.imageUrl}" alt="${singleProduct.altTxt}">`;
+      document.querySelector("#price").innerText += `${singleProduct.price}`;
+      document.querySelector(
+        "#description"
+      ).innerText += `${singleProduct.description}`;
+      document.querySelectorAll(
+        "option"
+      ).innerHTML += `${singleProduct.colors}`;
+    }
+  });
